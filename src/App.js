ff6472f3e8e17f7c6f87f26b020f1895f6c01d7e
Bobby Martin <robert.martin.619@gmail.com>
2019-04-30 15:41:33 -0700

    Add handleSearch to app and working api call

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 import Search from "./components/Search";
 
-import { handleSearch } from "./utils";
+import { API_URL, SEARCH_PARAM, API_KEY } from "./constants";
 
 import "./App.css";
 
@@ -11,13 +11,38 @@ const Container = styled.main`
   padding: 0 1.5rem;
 `;
 
-const App = () => (
-  <div className="App">
-    <Container>
-      <Search onSubmittingSearchTerms={handleSearch} />
-      <div />
-    </Container>
-  </div>
-);
+const App = () => {
+  const [gifs, setGifs] = useState(null);
+  console.log(gifs);
+
+  const handleSearch = async searchInput => {
+    const parsedSearchTerm = searchInput.split(" ").join("+");
+    const response = await fetch(
+      `${API_URL}${SEARCH_PARAM}${parsedSearchTerm}&api_key=${API_KEY}`
+    );
+    const data = await response.json();
+    setGifs(data.data);
+  };
+  return (
+    <div className="App">
+      <Container>
+        <Search onSubmittingSearchTerms={handleSearch} />
+        <div>
+          {gifs !== null && (
+            <>
+              {gifs.map(gif => (
+                <img
+                  key={gif.id}
+                  src={`${gif.images.original.url}`}
+                  alt={gif.title}
+                />
+              ))}
+            </>
+          )}
+        </div>
+      </Container>
+    </div>
+  );
+};
 
 export default App;
