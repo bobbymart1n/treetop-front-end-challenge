da77bbd02fc258804db4359eb05968b786455d2c
Bobby Martin <robert.martin.619@gmail.com>
2019-04-30 16:01:05 -0700

    Add different grid properties and container for grid

--- src/App.js
+++ src/App.js
@@ -9,11 +9,20 @@ import "./App.css";
 
 const Container = styled.main`
   padding: 0 1.5rem;
+  & .gifs-wrapper {
+    display: grid;
+    max-width: 100%;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 1rem;
+    & img {
+      box-sizing: border-box;
+      width: 100%;
+    }
+  }
 `;
 
 const App = () => {
   const [gifs, setGifs] = useState(null);
-  console.log(gifs);
 
   const handleSearch = async searchInput => {
     const parsedSearchTerm = searchInput.split(" ").join("+");
@@ -28,16 +37,22 @@ const App = () => {
       <Container>
         <Search onSubmittingSearchTerms={handleSearch} />
         <div>
+          {gifs === null && (
+            <h1>
+              Search for some gifs! They'll show here after submitting the
+              search.
+            </h1>
+          )}
           {gifs !== null && (
-            <>
+            <div className="gifs-wrapper">
               {gifs.map(gif => (
                 <img
                   key={gif.id}
-                  src={`${gif.images.original.url}`}
+                  src={`${gif.images.downsized_medium.url}`}
                   alt={gif.title}
                 />
               ))}
-            </>
+            </div>
           )}
         </div>
       </Container>
